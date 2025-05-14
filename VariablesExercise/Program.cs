742d1e22758ffecbc9509a33dfb5ba76eeaa010f
KillSwitch999 <140569515+KillSwitch999@users.noreply.github.com>
2025-05-14 15:45:49 -0400

    Initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "George";
+            int myAge = 25;
+            double myHeight = 5.9;
+            decimal myWeight = 160.5m;
+            char initial = 'G';
+            bool isStudent = true;
+
+            Console.WriteLine($"Name: {myName}\nAge: {myAge}\nHeight: {myHeight}\nWeight: {myWeight}\nInitial: {initial}\nIs TrueCoders Student: {isStudent}");
         }
     }
 }
